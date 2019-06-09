8c5d0fcb4d1f670b3daf3bc8dc3463efaa458959
TB <soupcoders@gmail.com>
2019-06-09 14:32:24 +0200

    Add bootstrap

--- src/form-preview/FormPreview.js
+++ src/form-preview/FormPreview.js
@@ -5,7 +5,7 @@ class FormPreview extends React.Component {
 
     render() {
         return (
-            <div className="FormPreview">
+            <div className="FormPreview col-8">
                 <p>This is form preview</p>
             </div>
         )
